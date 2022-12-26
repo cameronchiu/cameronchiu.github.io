93f701e2b5aff96a55b2afdc7cef78484d0ad356
Cameron Chiu <cameron2@andrew.cmu.edu>
2022-12-25 20:41:29 -0600

    AI Generated

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add your JavaScript code here
+
+// You can use this file to add interactivity to your website
+// For example, you can add event listeners to elements and execute code when an event occurs
+
+// Here is an example of adding a click event listener to a button element:
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  console.log('Button clicked!');
+});
